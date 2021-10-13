5961ee01700818183d642ba5705436f7305f3175
vickfreyre <vickfreyre@gmail.com>
2021-10-12 21:23:17 -0500

    Agregada la funcionalidad de generar CSV

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class AddressBook{
+class AddressBook {
 
     public static void main(String[] args) {
 
@@ -24,8 +24,7 @@ class AddressBook{
         System.out.println("                ******                                      ");
 
 
-
-        while(eleccion != 6){
+        while (eleccion != 6) {
 
             System.out.println("                         Ingrese la opción deseada:         ");
             System.out.println("1. Agregar contactos.");
@@ -39,7 +38,7 @@ class AddressBook{
             eleccion = ingresar.nextInt();
 
 
-            switch(eleccion){
+            switch (eleccion) {
                 case 1:
                     nombre = null;
                     telefono = null;
@@ -61,6 +60,7 @@ class AddressBook{
                 case 4:
                     System.out.println("Exportar CSV.");
 
+                    export(agenda);
 
                 case 5:
                     System.out.println("Recuperar agenda de un CSV.");
@@ -77,17 +77,17 @@ class AddressBook{
 
     static void create(String nombre, String telefono, HashMap<String, String> agenda) {
         Scanner ingresar = new Scanner(System.in);
-        System.out.print("Ingresa el nombre del contacto: " );
+        System.out.print("Ingresa el nombre del contacto: ");
         nombre = ingresar.nextLine();
         System.out.println();
 
-        System.out.print("Ingresa el teléfono del contacto: " );
+        System.out.print("Ingresa el teléfono del contacto: ");
         telefono = ingresar.nextLine();
         agenda.put(nombre, telefono);
         System.out.println();
     }
 
-     static void delete(String nombre, HashMap<String, String> agenda){
+    static void delete(String nombre, HashMap<String, String> agenda) {
         Scanner ingresar = new Scanner(System.in);
         System.out.print("Nombre del contacto a eliminar: ");
         nombre = ingresar.nextLine();
@@ -96,7 +96,49 @@ class AddressBook{
     }
 
 
-     static void list(HashMap<String, String> agenda){
+    static void list(HashMap<String, String> agenda) {
         System.out.println(agenda);
+    }
+
+    static void export(HashMap<String, String> agenda) {
+        File file = new File("C:/Users/vickf/IdeaProjects/AddressBook/src/output.csv");
+        BufferedWriter bf = null;
+        try {
+            bf = new BufferedWriter(new FileWriter("C:/Users/vickf/IdeaProjects/AddressBook/src/output.csv"));
+
+            for (Map.Entry<String, String> entrada :
+                    agenda.entrySet()) {
+
+                bf.write(entrada.getKey() + " : " + entrada.getValue());
+
+                bf.newLine();
+            }
+
+            bf.flush();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+
+                bf.close();
+            } catch (Exception e) {
+
+            }
         }
+    }
+
+    static void reader() {
+        try
+                (FileReader fr = new FileReader("C:/Users/vickf/Desktop/Tecmilenio/24 Optativa disciplinar II_Computación en Java(CU)/input.csv")) {
+            int c;
+
+            while ((c = fr.read()) != -1) System.out.print((char) c);
+
+        } catch (IOException e) {
+            System.out.println("Error de entrada/salida: " + e);
+        }
+    }
 }
